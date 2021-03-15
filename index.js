b57aee4d27f1ec96f1239b143c3b736ba4c9cb15
John Cyrill Corsanes <jccorsanes@protonmail.com>
2021-03-15 10:47:10 +0800

    refactor(workflow, server, index, second): second page initial ui

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { get } = server.router;
 const { render } = server.reply;
 
 // Launch server with options and a couple of routes
-server({ port: 3000 }, [
-  get('/', () => render('index.html'))
+server({ port: 8088 }, [
+  get('/', () => render('index.html')),
+  get('/second', () => render('second.html'))
 ]);
